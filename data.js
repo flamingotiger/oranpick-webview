dcdfa666497291a518354e9b199d01cd4e4f3705
flamingotiger <hhbhong@gmail.com>
2021-02-08 18:11:09 +0900

    dummy data

---
+++ data.js
@@ -0,0 +1,16 @@
+// 데이터 불러오는 구조
+
+const data = {
+  address_name: "서울 중구 다동 177-2",
+  category_group_code: "FD6",
+  category_group_name: "음식점",
+  category_name: "음식점 > 한식",
+  distance: "",
+  id: "1346301870",
+  phone: "02-777-4323",
+  place_name: "부민옥",
+  place_url: "http://place.map.kakao.com/1346301870",
+  road_address_name: "서울 중구 다동길 24-12",
+  x: "126.98031824515",
+  y: "37.5675038430283",
+};
